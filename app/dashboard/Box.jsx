2eeeb8799ec6bbb193f5454ec168796397a4b902
Surafel Araya <surafelay@gmail.com>
2023-12-31 11:10:41 +0300

    Create a box component

---
+++ app/dashboard/Box.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Box = ({ title }) => {
+  return (
+    <div className="p-4 border shadow hover:ring-2 hover:ring-offset-2 rounded-lg">
+      <h2 className="text-lg">{title}</h2>
+    </div>
+  );
+};
+
+export default Box;
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Box from "./Box";
 
 const Dashboard = () => {
   return (
     <div>
       <Navbar />
+
+      <div className="container mt-5">
+        <h1 className="font-bold text-xl text-center">Lively - Dashboard</h1>
+      </div>
+
+      <div className="container mx-auto grid sm:grid-cols-2 md:grid-cols-3 gap-6 mt-5">
+        <Box title={"catagory"} />
+        <Box title={"tags"} />
+        <Box title={"unit"} />
+        <Box title={"brand"} />
+        <Box title={"products"} />
+      </div>
     </div>
   );
 };
