d54deab5cb4b34b3631aec0915cf196b961ff0fc
Surafel Araya <surafelay@gmail.com>
2024-01-01 17:20:23 +0300

    Include Unravel logo

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -39,7 +39,11 @@ const Login = async () => {
   return (
     <div>
       <section className="bg-gray-50">
-        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="max-w-96 pt-8 mx-auto">
+          <img src="/unravel logo.svg" alt="" />
+        </div>
+
+        <div className="flex flex-col items-center mt-8 px-6 py-4 mx-auto md:h-screen lg:py-0">
           <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
